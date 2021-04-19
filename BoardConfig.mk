1a25063b59e9d694ad4b1fa3f3a52c6c0c2be201
František Kysela <bythedroid@gmail.com>
2021-04-19 15:04:03 +0200

    init repo

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -14,7 +14,7 @@
 # limitations under the License.
 #
 
-DEVICE_TREE_PATH := device/Huawei/anne
+DEVICE_TREE_PATH := device/huawei/anne
 
 #Architecture
 TARGET_ARCH := arm64
@@ -22,15 +22,50 @@ TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_ABI2 := 
 TARGET_CPU_VARIANT := generic
+TARGET_CPU_SMP := true
+
 
 TARGET_2ND_ARCH := arm
 TARGET_2ND_ARCH_VARIANT := armv7-a-neon
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := armv8-a
+
+
 TARGET_BOARD_PLATFORM := hi6250
 TARGET_BOOTLOADER_BOARD_NAME := hi6250
 
+TARGET_NO_BOOTLOADER := true
+TARGET_OTA_ASSERT_DEVICE := anne,hi6250
+
+# Kernel
+BOARD_KERNEL_BASE := 0x10000000
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_CUSTOM_BOOTIMG_MK := device/huawei/anne/custombootimg.mk
+BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x8000 --ramdisk_offset 0x01000000 --tags_offset 0x0100
+
+# phony empty kernel to satisfy build system, but this device does not
+# include a kernel in the recovery image -- flash to recovery_ramdisk
+TARGET_PREBUILT_KERNEL := device/huawei/blanc/dummykernel
+
+# File System
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 33554432
+
+# NOTE I only bother properly setting recovery image size
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 33554432
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 3909091328
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 54316236800
+BOARD_FLASH_BLOCK_SIZE := 131072 # (BOARD_KERNEL_PAGESIZE * 64)
+BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
+
+
+TARGET_EXFAT_DRIVER := exfat
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
+
+# Fstab and init.rc files
+TARGET_RECOVERY_FSTAB := $(DEVICE_TREE_PATH)/recovery.fstab
+TARGET_RECOVERY_DEVICE_DIRS += $(DEVICE_TREE_PATH)
 
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
@@ -38,3 +73,29 @@ TARGET_USERIMAGES_USE_VFAT := true
 TARGET_USERIMAGES_USE_EXFAT := true
 TARGET_USERIMAGES_USE_SQUASHFS := true
 
+BOARD_HAS_NO_SELECT_BUTTON := true
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
+TARGET_USES_MKE2FS := true
+TW_INCLUDE_FUSE_EXFAT := true
+TW_INCLUDE_NTFS_3G := true
+
+TW_THEME := portrait_hdpi
+BOARD_SUPPRESS_SECURE_ERASE := true
+BOARD_HAS_NO_REAL_SDCARD := true
+RECOVERY_SDCARD_ON_DATA := true
+TW_EXCLUDE_DEFAULT_USB_INIT := true
+TW_EXCLUDE_SUPERSU := true
+TW_INCLUDE_CRYPTO := true
+TW_INCLUDE_CRYPTO_FBE := true
+TW_BRIGHTNESS_PATH := /sys/class/leds/lcd_backlight0/brightness
+TW_NO_SCREEN_BLANK := true
+TW_DEFAULT_BRIGHTNESS := "2048"
+TW_CUSTOM_BATTERY_PATH := /sys/class/power_supply/Battery
+
+TW_USE_NEW_MINADBD := true
+TW_DEVICE_VERSION := $(shell date -u +"%Y%m%d")
+
+# Selinux
+SELINUX_IGNORE_NEVERALLOWS := true
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
+include vendor/omni/sepolicy/sepolicy.mk
