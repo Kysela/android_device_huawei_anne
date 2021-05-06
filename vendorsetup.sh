a3804705eec08363f7dcd3256d2d826c461e498b
František Kysela <bythedroid@gmail.com>
2021-05-06 16:58:54 +0200

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,5 @@
-COMMON_LUNCH_CHOICES := omni_anne-eng omni_anne-userdebug
-add_lunch_combo omni_anne-eng
+
 export ALLOW_MISSING_DEPENDENCIES=true
+
+add_lunch_combo omni_anne-eng
+add_lunch_combo omni_anne-userdebug
